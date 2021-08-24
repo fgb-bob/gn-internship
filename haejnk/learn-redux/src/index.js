704d35d4187adda52867ec5b775761d3f16383f7
HaeJnk <ramong4028@gmail.com>
2021-08-24 17:58:52 +0900

    #2 리액트 스터디 (실습)
    6장

---
+++ haejnk/learn-redux/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
+
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어 생성
+// console.log(store.getState()); // 스토어의 상태 확인
+// composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+serviceWorker.unregister();
