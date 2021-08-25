0a46cfe68e8f39b39ebf5a43854ae9c5c5fa0bcc
ditto-young <gpdud0243@gmail.com>
2021-08-25 18:01:17 +0900

    #1 8.25 7장 리덕스 미들웨어 실습폴더

--- young/react_ex/learn-redux-middleware/src/index.js
+++ young/react_ex/learn-redux-middleware/src/index.js
@@ -5,25 +5,43 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 import logger from 'redux-logger';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import createSagaMiddleware from '@redux-saga/core';
+
+const customHistory = createBrowserHistory();
+const SagaMiddleware = createSagaMiddleware({
+  context : {
+    history : customHistory
+  }
+}); //사가 미들웨어를 만듭니다.
 
 const store = createStore(
   rootReducer, 
-  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+  composeWithDevTools(
+    applyMiddleware(
+      ReduxThunk.withExtraArgument({history: customHistory}),
+      SagaMiddleware,
+      logger
+    )
+  )
 );
 // 여러개의 미들웨어를 적용할 수 있습니다.
 
+SagaMiddleware.run(rootSaga); //루트 사가를 실행해줍니다.
+// 주의 : 스토어 생성이 된 다음에 위 코드를 실행해야함.
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById('root')
+  <Router history={customHistory}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      </Router>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
